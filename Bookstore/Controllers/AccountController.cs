60b02cc7e75405b541348b13d41ef7fdfde2d45d
kylan120 <142834131+kylan120@users.noreply.github.com>
2024-04-17 10:26:01 -0500

    did steps 21-28, not sure if i marked 16-20 as completed

--- Bookstore/Controllers/AccountController.cs
+++ Bookstore/Controllers/AccountController.cs
@@ -29,7 +29,7 @@ namespace Bookstore.Controllers
 
             if (ModelState.IsValid)
             {
-                var user = new Bookstore.Models.User { UserName = model.Username };
+                var user = new Bookstore.Models.User { UserName = model.Username, FirstName = model.FirstName, LastName = model.LastName, Email = model.Email };
                 var result = await userManager.CreateAsync(user, model.Password);
 
                 if (result.Succeeded)
--- Bookstore/Models/DomainModels/User.cs
+++ Bookstore/Models/DomainModels/User.cs
@@ -6,6 +6,8 @@ namespace Bookstore.Models
 {
     public class User : IdentityUser
     {
+        public string FirstName {  get; set; }
+        public string LastName { get; set; }
         [NotMapped]
         public IList<string> RoleNames { get; set; }
     }
